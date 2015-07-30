7b28d54d36e71642b2601c87fb6b06bf1c1d2422
Camden Segal <camden.segal@gmail.com>
2015-07-30 17:37:58 -0400

    Tweak player movement and grabbing

--- Assets/Scripts/ScoreZone.cs
+++ Assets/Scripts/ScoreZone.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class ScoreZone : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	void OnTriggerEnter2D(Collider2D other) {
 
 		if(other.attachedRigidbody.gameObject.tag == "SpawnItem")
