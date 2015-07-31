e2aa52c46c0fb79a3a8bd614a3bdbf1b13b66425
Camden Segal <camden.segal@gmail.com>
2015-07-30 23:05:37 -0400

    It's an adventure now

---
+++ Assets/Scripts/LeaveScreenTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeaveScreenTrigger : MonoBehaviour {
+
+	bool triggered = false;
+
+	void OnTriggerEnter2D( Collider2D other ) {
+		if ( !triggered && other.attachedRigidbody.tag == "Player" ) {
+			Camera.main.GetComponent<SmoothFollowCamera>().enabled = true;
+			triggered = true;
+		}
+	}
+}
---
+++ Assets/Scripts/SmoothFollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollowCamera : MonoBehaviour {
+	
+	public Transform target;
+	Transform t;
+
+	// Use this for initialization
+	void Start () {
+		t = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 tpos = target.position;
+		tpos.z = t.position.z;
+		t.position = Vector3.Lerp( t.position, tpos, Time.deltaTime );
+	}
+}
