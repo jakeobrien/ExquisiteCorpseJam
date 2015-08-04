4ed8a7b6d49dc8b6da4742c1dc9a5c7212659633
Andrei Marks <hey@ndrei.us>
2015-08-03 21:27:51 -0400

    sticky balls.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour 
+{
+    public int maxRings;
+    private int _ring;
+    private Rigidbody2D _rb;
+    private Transform _playerTransform;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if ( _playerTransform == null ) return;
+        MoveTowardPlayer();
+    }
+
+    /*
+    void OnCollisionEnter2D( Collision2D col )
+    {
+        Ball ball = col.transform.GetComponent<Ball>();
+        if ( ball == null || ball.IsStickingToPlayer() ) return;
+
+        ball.StickToPlayer( this.transform );
+    }
+    */
+    private void MoveTowardPlayer()
+    {
+        float playerRadius = _playerTransform.GetComponent<CircleCollider2D>().radius * 2.5f ;
+        Vector3 attachmentPoint = _playerTransform.position + ( transform.position - _playerTransform.position ).normalized * playerRadius;
+
+        Vector3 direction = ( attachmentPoint - transform.position );
+        _rb.AddForce( direction, ForceMode2D.Impulse );
+    }
+
+    public void StickToPlayer( Transform playerTransform )
+    {
+        _playerTransform = playerTransform; 
+    }
+
+    public bool IsStickingToPlayer()
+    {
+        return _playerTransform != null;
+    }
+}
---
+++ Assets/Scripts/BallHolder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BallHolder : MonoBehaviour 
+{
+    private List<Ball> __balls;
+    private List<Ball> _balls
+    {
+        get
+        {
+            if ( __balls == null ) __balls = new List<Ball>();
+            return __balls;
+        }
+    }
+
+    void OnCollisionEnter2D( Collision2D col )
+    {
+        Ball ball = col.transform.GetComponent<Ball>();
+        if ( ball == null || _balls.Contains( ball ) ) return;
+
+        ball.StickToPlayer( this.transform );
+    }
+}
